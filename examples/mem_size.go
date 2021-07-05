99be393a968451fddfe49af73fbcb08cb58084b6
miiniper <miiniper@gmail.com>
2021-07-05 20:39:26 +0800

    Size and alignment guarantees

---
+++ examples/mem_size.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"unsafe"
+)
+
+// Event 参考地址 https://golang.org/ref/spec#Size_and_alignment_guarantees
+
+type Demo1 struct {
+	NS      bool  `json:"ns"`      //1
+	ID      int   `json:"id"`      //8
+	Message int16 `json:"message"` //2
+}
+
+type Demo2 struct {
+	NS      bool  `json:"ns"`      //1
+	Message int16 `json:"message"` //2
+	ID      int   `json:"id"`      //8
+}
+
+type Demo3 struct {
+	NS      bool  `json:"ns"`      //1
+	Message int16 `json:"message"` //2
+	ID      int   `json:"id"`      //8
+	Host    Demo2 `json:"host"`    //16
+}
+
+type Demo4 struct {
+	ID      int   `json:"id"`      //8
+	NS      bool  `json:"ns"`      //1
+	Host    Demo2 `json:"host"`    //16
+	Message int16 `json:"message"` //2
+}
+
+func main() {
+	fmt.Println("service starting ... ")
+	fmt.Println("Demo1 size :", unsafe.Sizeof(Demo1{})) //Demo1 size : 24
+	fmt.Println("Demo2 size :", unsafe.Sizeof(Demo2{})) //Demo2 size : 16
+	fmt.Println("Demo3 size :", unsafe.Sizeof(Demo3{})) //Demo3 size : 32
+	fmt.Println("Demo4 size :", unsafe.Sizeof(Demo4{})) //Demo4 size : 40
+
+}
